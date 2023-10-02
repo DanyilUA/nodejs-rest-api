2899b93d75d5a2124f81a47cfc73e45ffd58ea2b
DanyilUA <122489621+DanyilUA@users.noreply.github.com>
2023-10-02 23:06:43 +0200

    v1

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,74 @@
 const express = require('express')
 
 const router = express.Router()
+const { HttpError } = require('../../helpers/index');
+const Joi = require('joi');
+
+const contactsService = require('../../models/contacts');
+
+
+const contactSchema = Joi.object({
+  name: Joi.string().required().messages({
+    'any.required': `"name" is a required field`,
+  }),
+  email: Joi.string().required().messages({
+    'any.required': `"email" is a required field`,
+  }),
+  phone: Joi.number().required().messages({
+    'any.required': `"phone" is a required field`,
+  }),
+});
+
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message number 2' })
-})
+  try {
+    const result = await contactsService.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const { contactId } = req.params;
+    const result = await contactsService.getContactById(contactId);
+    if (!result) {
+      throw HttpError(404, `Contact ${contactId} not found`);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const { error } = contactSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const result = await contactsService.addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    
+  } catch (error) {
+        next(error);
+  }
+});
+
+router.delete('/:contactId', async (req, res, next) => {
+    try {
+      
+    } catch (error) {
+          next(error);
+    }
+});
 
 module.exports = router
