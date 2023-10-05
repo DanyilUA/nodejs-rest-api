0d573039d1bba62d8489fd40cd9cbb091ac7fa20
DanyilUA <122489621+DanyilUA@users.noreply.github.com>
2023-10-05 20:27:44 +0200

    fixed midleware

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,19 +1,6 @@
 const { HttpError } = require('../helpers/index');
-const Joi = require('joi');
-
 const contactsService = require('../models/contacts');
 
-const contactSchema = Joi.object({
-  name: Joi.string().required().messages({
-    'any.required': `missing required name field`,
-  }),
-  email: Joi.string().required().messages({
-    'any.required': `missing required email field`,
-  }),
-  phone: Joi.number().required().messages({
-    'any.required': `missing required phone field`,
-  }),
-});
 
 const getAllContacts = async (req, res, next) => {
     try {
@@ -39,11 +26,6 @@ const getContact = async (req, res, next) => {
 
 const createContact = async (req, res, next) => {
   try {
-
-    const { error } = contactSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
     const result = await contactsService.addContact(req.body);
     res.status(201).json(result);
   } catch (error) {
@@ -54,10 +36,6 @@ const createContact = async (req, res, next) => {
 const updateContactById = async (req, res, next) => {
   try {
 
-    const { error } = contactSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
     const { contactId } = req.params;
 
     const result = await contactsService.updateContact(contactId, req.body);
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,17 +1,18 @@
 const express = require('express')
 
 const router = express.Router()
-const contactsController = require("../../controllers/contact-controller");
+const contactsController = require("../../controllers/contact");
 const { isEmptyBody } = require('../../middlewares/index');
+const { validationBody } = require('../../middlewares/index');
 
 
 router.get('/', contactsController.getAllContacts);
 
 router.get('/:contactId', contactsController.getContact);
 
-router.post('/', isEmptyBody ,contactsController.createContact);
+router.post('/', isEmptyBody, validationBody, contactsController.createContact);
 
-router.put('/:contactId', isEmptyBody, contactsController.updateContactById);
+router.put('/:contactId', isEmptyBody, validationBody, contactsController.updateContactById);
 
 router.delete('/:contactId', contactsController.deleteContact);
 
