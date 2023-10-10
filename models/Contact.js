5ecb440b670b620cec2593874294feac37b2a4cd
DanyilUA <122489621+DanyilUA@users.noreply.github.com>
2023-10-10 20:20:54 +0200

    fixed

--- models/Contact.js
+++ models/Contact.js
@@ -25,10 +25,10 @@ const contactSchemaJoi = Joi.object({
   name: Joi.string().required().messages({
     'any.required': 'missing required name field',
   }),
-  email: Joi.string().messages({
+  email: Joi.string().required().messages({
     'any.required': 'missing required email field',
   }),
-  phone: Joi.string().messages({
+  phone: Joi.string().required().messages({
     'any.required': 'missing required phone field',
   }),
   favorite: Joi.boolean(),
